f5d1a67bc4ae4370e85e76d5290e8c5786c1c124
unknown <ttomita@ttomita00.knowledgebeans.local>
2016-04-13 16:39:02 +0900

    change to ws

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ io.sockets.on( "connection", function ( socket ) {
 });
 
 */
-/*
 var port = process.env.PORT || 1337;
 var WebSocketServer = require('ws').Server
     , http = require('http')
@@ -63,9 +62,11 @@ function broadcast(message) {
  
 server.listen(port);
 */
+/*
 var http = require('http');
 var port = process.env.port || 1337;
 http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type':'text/plain'});
     res.end('Hello World\n');
 }).listen(port);
+*/
